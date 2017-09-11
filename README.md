7655cee62125f6f98441e29d64def9de28605398
Joel Ross <joelross@uw.edu>
2017-09-11 10:08:59 -0700

    Initial completed code (autumn 2017)

---
+++ README.md
@@ -0,0 +1,16 @@
+# Java Swing Demo
+
+This repository contains code examples demonstrating Java GUIs with Swing. See [the accompanying book chapter](https://info448.github.io/java-swing.html) for more details.
+
+## Running the Demo
+Build and run this demo with
+
+```
+# on Mac/Linux
+./gradlew -q run
+
+# on Windows
+gradlew -q run
+```
+
+Enable the [Gradle Daemon](https://docs.gradle.org/current/userguide/gradle_daemon.html) for faster build times.
---
+++ src/main/java/MyGUI.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+
+/**
+ * A demo of GUI programming with Swing
+ */
+public class MyGUI extends JFrame {
+	
+	public MyGUI() {
+		super("Java Swing Demo");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton theButton = new JButton("Click me!");
+		theButton.addActionListener(new ActionListener() {
+	  	//class declaration goes in here!
+
+	  	public void actionPerformed(ActionEvent event) { 
+	  		System.out.println("You clicked me!");
+	  	}
+		});
+
+		JButton otherButton = new JButton("No, click me!");
+		otherButton.addActionListener(new ActionListener(){
+			//class body goes here
+			public void actionPerformed(ActionEvent e){
+				System.out.println("haha I win");
+			}
+		});
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		
+		panel.add(theButton);
+		panel.add(otherButton);
+		
+		this.add(panel);
+				
+		this.pack();
+		this.setVisible(true);		
+	}
+	
+	public static void main(String[] args) {
+    new MyGUI(); //create the frame
+  }
+}
